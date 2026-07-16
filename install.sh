978d59a3a23750941c5269a9e949978e13206329
KitekXP <tomek.wojtczak@gmail.com>
2026-07-16 16:18:21 +0200

    Fixed some coloring bugs in /src/builtin.sh, Added logging to the install script, The installation script now removes the $INSTALL_PATH

--- install.sh
+++ install.sh
@@ -2,11 +2,14 @@
 
 # shellcheck disable=SC2059
 
+. "./src/builtin.sh"
+
 gen_wrapper() {
 	printf "$(cat ./templates/wrapper.sh.template)" "$SHELL" "$1" > "$1/wrapper.sh"
 }
 
 install_kittybuild() {
+	info "Checking env overrides..."
 	if [ ! -n "${INSTALL_PATH+x}" ]
 	then
 		local INSTALL_PATH="$HOME"/.tools/kittybuild
@@ -17,20 +20,28 @@ install_kittybuild() {
 		local BIN_PATH="$HOME"/.local/bin
 	fi
 
+	info "Setting variables..."
 	local EXEC_NAME="kittybuild"
 
 
 	# But is it possible with accurate hitboxes?
+	warn "Cleaning installation dir: $INSTALL_PATH..."
 	rm -rf "$INSTALL_PATH"
+	info "Creating installation dir: $INSTALL_PATH..."
 	mkdir -p "$INSTALL_PATH" "$BIN_PATH"
 
+	info "Generating the wrapper script..."
 	gen_wrapper "$INSTALL_PATH"
 	chmod +x "$INSTALL_PATH"/wrapper.sh
 
+	info "Copying other scripts"
 	cp -r src/* "$INSTALL_PATH"
 	chmod +x "$BIN_PATH"/*
 
+	info "Creating a symbolic link for the wrapper: $BIN_PATH/$EXEC_NAME..."
 	ln -sf "$INSTALL_PATH"/wrapper.sh "$BIN_PATH/$EXEC_NAME"
+
+	info "Installed successfuly!"
 }
 
 install_kittybuild "$@"
--- src/builtin.sh
+++ src/builtin.sh
@@ -3,15 +3,15 @@ get_time() {
 }
 
 info() {
-	printf "\033[0m[\033[0m%s\033[1m/\033[0;34mINFO]\033[0m %s%s%s%s%s\n" "$(get_time)" "$@"
+	printf "[%s\033[1m/\033[0;34mINFO\033[0m] %s%s%s%s%s\n" "$(get_time)" "$@"
 }
 
 warn() {
-	printf "\033[33m[\033[0m%s\033[1m/\033[0;33mWARNING]\033[0m %s%s%s%s%s\n" "$(get_time)" "$@"
+	printf "[%s\033[1m/\033[0;33mWARNING\033[0m] %s%s%s%s%s\n" "$(get_time)" "$@"
 }
 
 error() {
-	printf "\033[31m[\033[0m%s\033[1m/\033[0;31mERROR]\033[0m %s%s%s%s%s\n" "$(get_time)" "$@"
+	printf "[%s\033[1m/\033[0;31mERROR\033[0m] %s%s%s%s%s\n" "$(get_time)" "$@"
 }
 
 fatal_error() {
