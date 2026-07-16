9f265cc35b9a4cdf46d13667dbf7f98255aa4cad
KitekXP <tomek.wojtczak@gmail.com>
2026-07-16 16:10:08 +0200

    Fixed the installation script

--- install.sh
+++ install.sh
@@ -9,27 +9,28 @@ gen_wrapper() {
 install_kittybuild() {
 	if [ ! -n "${INSTALL_PATH+x}" ]
 	then
-		local INSTALL_PATH="$HOME/.tools/kittybuild"
+		local INSTALL_PATH="$HOME"/.tools/kittybuild
 	fi
 
 	if [ ! -n "${BIN_PATH+x}" ]
 	then
-		local BIN_PATH="$HOME/.local/bin"
+		local BIN_PATH="$HOME"/.local/bin
 	fi
 
 	local EXEC_NAME="kittybuild"
 
 
 	# But is it possible with accurate hitboxes?
+	rm -rf "$INSTALL_PATH"
 	mkdir -p "$INSTALL_PATH" "$BIN_PATH"
 
 	gen_wrapper "$INSTALL_PATH"
-	chmod +x "$INSTALL_PATH/wrapper.sh"
+	chmod +x "$INSTALL_PATH"/wrapper.sh
 
 	cp -r src/* "$INSTALL_PATH"
-	chmod +x "$BIN_PATH/*"
+	chmod +x "$BIN_PATH"/*
 
-	ln -sf "$INSTALL_PATH/wrapper.sh" "$BIN_PATH/$EXEC_NAME"
+	ln -sf "$INSTALL_PATH"/wrapper.sh "$BIN_PATH/$EXEC_NAME"
 }
 
 install_kittybuild "$@"
